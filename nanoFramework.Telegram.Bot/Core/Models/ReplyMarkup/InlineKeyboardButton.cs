42631fb8d62164628881566611ee81462c68ba59
Naglfario <135315484+Naglfario@users.noreply.github.com>
2025-06-21 15:06:12 +0300

    Inline keyboard support (#5)
    * refactoring
    
    * InlineKeyboardMarkup support added

---
+++ nanoFramework.Telegram.Bot/Core/Models/ReplyMarkup/InlineKeyboardButton.cs
@@ -0,0 +1,51 @@
+﻿namespace nanoFramework.Telegram.Bot.Core.Models.ReplyMarkup
+{
+    /// <summary>
+    /// This object represents one button of an inline keyboard. 
+    /// Exactly one of the optional fields must be used to specify type of the button.
+    /// </summary>
+    public class InlineKeyboardButton
+    {
+        /// <summary>
+        /// Label text on the button
+        /// </summary>
+        public string text { get; set; } = "Can't be empty!";
+
+        /// <summary>
+        /// Optional. HTTP or tg:// URL to be opened when the button is pressed. 
+        /// Links tg://user?id=<user_id> can be used to mention a user by their 
+        /// identifier without using a username, if this is allowed by their privacy settings.
+        /// </summary>
+        public string url { get; set; } = null;
+
+        /// <summary>
+        /// Optional. Data to be sent in a callback query to the bot when 
+        /// the button is pressed, 1-64 bytes
+        /// </summary>
+        public string callback_data { get; set; } = null;
+
+        /// <summary>
+        /// Optional. If set, pressing the button will prompt the user to select 
+        /// one of their chats, open that chat and insert the bot's username and 
+        /// the specified inline query in the input field. May be empty, 
+        /// in which case just the bot's username will be inserted. 
+        /// Not supported for messages sent on behalf of a Telegram Business account.
+        /// </summary>
+        public string switch_inline_query { get; set; } = null;
+
+        /// <summary>
+        /// Optional. If set, pressing the button will insert the bot's username 
+        /// and the specified inline query in the current chat's input field. 
+        /// May be empty, in which case only the bot's username will be inserted.
+        /// This offers a quick way for the user to open your bot in inline mode 
+        /// in the same chat - good for selecting something from multiple options. 
+        /// Not supported in channels and for messages sent on behalf of a Business account.
+        /// </summary>
+        public string switch_inline_query_current_chat { get; set; } = null;
+
+        /// <summary>
+        /// Optional. Description of the button that copies the specified text to the clipboard.
+        /// </summary>
+        public CopyTextButton copy_text { get; set; } = null;
+    }
+}
--- nanoFramework.Telegram.Bot/Core/Providers/URLProvider.cs
+++ nanoFramework.Telegram.Bot/Core/Providers/URLProvider.cs
@@ -9,9 +9,20 @@ namespace nanoFramework.Telegram.Bot.Core.Providers
         private readonly ISettingsProvider _settings;
 
         internal const string TelegramBaseUrl = "https://api.telegram.org/bot";
-        internal const string GetUpdatesRoute = "/getUpdates?allowed_updates=[%22message%22]";
-        internal const string OffsetParam = "&offset=";
+        internal const string GetUpdatesRoute = "/getUpdates?";
+        internal const string AllowUpdatesStart = "allowed_updates=[";
+        internal const string TrackMessages = "%22message%22";
+        internal const string TrackCallbackQuery = "%22callback_query%22";
+        internal const string OffsetParam = "offset=";
         internal const string SendMessageRoute = "/sendMessage?";
+
+        internal const string ChatIdParam = "chat_id=";
+        internal const string TextParam = "&text=";
+        internal const string DisableNotificationParam = "&disable_notification=";
+        internal const string ProtectContentParam = "&protect_content=";
+        internal const string ParseModeParam = "&parse_mode=";
+        internal const string ReplyParametersParam = "&reply_parameters=";
+        internal const string ReplyMarkupParam = "&reply_markup=";
 
         public URLProvider(ISettingsProvider settings)
         {
@@ -23,6 +34,20 @@ namespace nanoFramework.Telegram.Bot.Core.Providers
             var sb = new StringBuilder(TelegramBaseUrl);
             sb.Append(_settings.Token);
             sb.Append(GetUpdatesRoute);
+            if(_settings.TrackMessages || _settings.TrackCallbackQuery)
+            {
+                sb.Append(AllowUpdatesStart);
+                if(_settings.TrackMessages && _settings.TrackCallbackQuery)
+                {
+                    sb.Append(TrackMessages);
+                    sb.Append(',');
+                    sb.Append(TrackCallbackQuery);
+                }
+                else if(_settings.TrackMessages) sb.Append(TrackMessages);
+                else if(_settings.TrackCallbackQuery) sb.Append(TrackCallbackQuery);
+                sb.Append(']');
+                sb.Append('&');
+            }
             if (lastUpdateId > 0)
             {
                 sb.Append(OffsetParam);
@@ -36,26 +61,26 @@ namespace nanoFramework.Telegram.Bot.Core.Providers
             var sb = new StringBuilder(TelegramBaseUrl);
             sb.Append(_settings.Token);
             sb.Append(SendMessageRoute);
-            sb.Append("chat_id="); sb.Append(command.chat_id);
-            sb.Append("&text="); sb.Append(command.text);
-            sb.Append("&disable_notification="); sb.Append(command.disable_notification);
-            sb.Append("&protect_content="); sb.Append(command.protect_content);
+            sb.Append(ChatIdParam); sb.Append(command.chat_id);
+            sb.Append(TextParam); sb.Append(command.text);
+            sb.Append(DisableNotificationParam); sb.Append(command.disable_notification);
+            sb.Append(ProtectContentParam); sb.Append(command.protect_content);
 
             if (!string.IsNullOrEmpty(command.parse_mode))
             {
-                sb.Append("&parse_mode="); sb.Append(command.parse_mode);
+                sb.Append(ParseModeParam); sb.Append(command.parse_mode);
             }
 
             if (command.reply_parameters != null)
             {
                 var replyParametersJson = JsonSerializer.SerializeObject(command.reply_parameters, false);
-                sb.Append("&reply_parameters="); sb.Append(replyParametersJson);
+                sb.Append(ReplyParametersParam); sb.Append(replyParametersJson);
             }
 
             if (command.reply_markup != null)
             {
                 var replyMarkupJson = JsonSerializer.SerializeObject(command.reply_markup, false);
-                sb.Append("&reply_markup="); sb.Append(replyMarkupJson);
+                sb.Append(ReplyMarkupParam); sb.Append(replyMarkupJson);
             }
 
             return sb.ToString();
